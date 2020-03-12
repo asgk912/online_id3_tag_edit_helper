eaaf5e2f1000e35703b878d1da60d60e7485fad1
Albert <asgk912@gmail.com>
2020-03-12 09:51:43 -0700

    module for file upload

---
+++ client/comp/ChooseFile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button } from 'react-bootstrap';
+
+const ChooseFile = React.forwardRef(
+  function cf({uploadFileOnClick}, ref) {
+    return (
+      <div>
+        <h4>Choose an audio file to edit tags</h4>
+        <Form>
+          <Form.Group controlId="audioFilePath">
+            <Form.Control ref={ref} type="file" accept="audio/*" />
+          </Form.Group>
+          <Button variant="outline-info" size="sm" onClick={uploadFileOnClick}>Upload File</Button>
+        </Form>
+      </div>
+    )
+  }
+);
+
+ChooseFile.proptypes = {
+  uploadFileOnClick: PropTypes.func
+}
+
+export default ChooseFile;
