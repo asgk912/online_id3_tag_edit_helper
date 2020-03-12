84549aa1f1354a1ec41af887feec4f19613e683c
Albert <asgk912@gmail.com>
2020-03-12 09:52:34 -0700

    module to intake string inputs to search through spotify api

---
+++ client/comp/SearchForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button } from 'react-bootstrap';
+
+export default function SearchForm({ searchOnApi }) {
+  const [title, setTitle] = useState('');
+  const [artist, setArtist] = useState('');
+
+  let handleOnChange = (e) => {
+    if(e.target.id === 'title'){
+      setTitle(e.target.value);
+    } else {
+      setArtist(e.target.value);
+    }
+  };
+
+  return (
+    <div>
+      <br />
+      <h4>Search Song by Title and Artist</h4>
+      <Form>
+        <Form.Group>
+          <Form.Control id='artist' type="string" placeholder="Artist Name" value={artist} onChange={handleOnChange}/>
+          <Form.Control id='title' type="string" placeholder="Song Title" value={title} onChange={handleOnChange} />
+          <Button variant="outline-info" size="sm" onClick={()=>{searchOnApi(title, artist)}}>Search</Button>
+        </Form.Group>
+      </Form>
+    </div>
+  )
+}
+
+SearchForm.proptypes = {
+  searchOnApi: PropTypes.func
+};
