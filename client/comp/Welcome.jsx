3967429e49a569b50cafac11b69d2315b22f2b17
Albert <asgk912@gmail.com>
2020-03-26 16:44:42 -0700

    added icon from font aweomse 5

--- client/comp/Welcome.jsx
+++ client/comp/Welcome.jsx
@@ -1,15 +1,21 @@
+// node packages
 import React from 'react';
 import PropTypes from 'prop-types';
+// style
+import { Title, Info } from './style.jsx';
 
-const Welcome = ({ pageControlOnClick }) => {
+export default function Welcome({ pageControlOnClick }) {
   return (
     <div>
-      <div className='recognition' onClick={pageControlOnClick}>
-        Audoio Recognition Method
-      </div>
-      <div className='onlineSearch' onClick={pageControlOnClick}>
-        Online Search Method
-      </div>
+      <Title> <i className='far fa-file-audio'/> Online ID3 Tag Edit Helper</Title>
+      <Info>
+        <div className='recognition' onClick={pageControlOnClick}>
+          Audoio Recognition Method
+        </div>
+        <div className='onlineSearch' onClick={pageControlOnClick}>
+          Online Search Method
+        </div>
+      </Info>
     </div>
   );
 }
@@ -17,5 +23,3 @@ const Welcome = ({ pageControlOnClick }) => {
 Welcome.propTypes = {
   pageControlOnClick: PropTypes.func
 };
-
-export default Welcome;
