607e571f8a22b1a75d0b1a4f0f2b4c557d962728
Albert <asgk912@gmail.com>
2020-03-27 15:32:43 -0700

    updated to use page controller submodules

--- client/comp/App.jsx
+++ client/comp/App.jsx
@@ -1,66 +1,28 @@
 // npm packages
-import React, {useState} from 'react';
-import { Button } from 'react-bootstrap';
-import axios from 'axios';
-
+import React, { useState } from 'react';
 // submodules
-import ChooseFile from './ChooseFile.jsx';
-import SearchForm from './SearchForm.jsx';
+import Welcome from './Welcome.jsx';
+import EditHelper from './EditHelper.jsx';
 
-function App() {
-  const [step, setStep] = useState(3);
-  var fileInputRef = React.createRef();
+export default function App() {
+  const [page, setPage] = useState(0);
 
-  /*
-    STAR: Declare Event Listeners
-  */
- var uploadFileOnClick = () => {
-    var data = new FormData();
-    data.append('audio', fileInputRef.current.files[0]);
-
-    axios.post('/api/v1/file', data, {
-      header: {
-        'Content-Type': 'multipart/form-data',
-      }
-    })
-      .then(() => {
-        setStep(2);
-      })
-      .catch((err) => console.log(err));
-  }
-
-  let searchOnApi = (artist, title) => {
-    let config = {}
-    config.params = {
-      term: `${artist.trim()} ${title.trim()}`
+  let pageControlOnClick = (e) => {
+    switch(e.target.className){
+      case 'welcome':
+        setPage(0);
+        break;
+      case 'recognition':
+        setPage(1);
+        break;
+      default:
+        setPage(0);
     }
-
-    axios.get('/api/v1/search', config)
-      .then((res) => console.log(res.data))
-      .catch((e) => console.log(e));
-  };
-
-  let downloadFileOnClick = () => {
-    axios.get('/api/v1/file')
-      .then(() => {
-        window.open('/api/v1/file');
-      })
-      .catch((err) => console.log(err));
   }
-  /*
-    END: Declare Event Listeners
-  */
-
-  let step2 = (step > 1) ? <SearchForm searchOnApi={searchOnApi}/>: '';
-  // let step3 = (step > 2) ? <SearchForm searchOnApi={searchOnApi}/>: '';
 
   return (
     <div>
-      <ChooseFile ref={fileInputRef} uploadFileOnClick={uploadFileOnClick}/>
-      {step2}
-      <Button onClick={downloadFileOnClick}>Download File</Button>
+      {page === 0 ? <Welcome pageControlOnClick={pageControlOnClick}/> : <EditHelper />}
     </div>
   );
 }
-
-export default App;
