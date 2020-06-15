3471dd1fb2b9a8288dd556fa832336042862b690
Albert <asgk912@gmail.com>
2020-06-15 14:28:18 -0700

    created and imported step2 module

--- client/comp/EditWindow.jsx
+++ client/comp/EditWindow.jsx
@@ -4,17 +4,22 @@ import PropTypes from 'prop-types';
 // submodules
 import NavigationBar from './NavigationBar.jsx';
 import Step1_FileUpload from './Step1_FileUpload.jsx';
+import Step2_SearchInfo from './Step2_SearchInfo.jsx';
 
 export default function EditWindow({ pageControlOnClick }) {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(1);
+
+  let nextStepOnClick = () => {
+    setStep(step+1);
+  }
 
   return (
     <div>
       <NavigationBar step={step} pageControlOnClick={pageControlOnClick} />
-      <Step1_FileUpload />
-      {(step >= 2) ? <div>Step 2</div> : ''}
-      {(step >= 3) ? <div>Step 3</div> : ''}
-      {(step >= 4) ? <div>Step 4</div> : ''}
+      <Step1_FileUpload step={step} setStep={setStep} />
+      {(step > 1) ? <Step2_SearchInfo nextStepOnClick={nextStepOnClick} /> : ''}
+      {(step > 2) ? <div>Step 3</div> : ''}
+      {(step > 3) ? <div>Step 4</div> : ''}
     </div>
   );
 }
---
+++ client/comp/Step2_SearchInfo.jsx
@@ -0,0 +1,11 @@
+// node packages
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+export default function Step2_SearchInfo( {nextStepOnClick} ) {
+  return (<div>Step 2 Initial</div>);
+}
+
+Step2_SearchInfo.propTypes = {
+  nextStepOnClick: PropTypes.func
+}
