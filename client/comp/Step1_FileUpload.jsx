1d0d03649e2d0ba746c950b15ac682cff5a980fc
Albert <asgk912@gmail.com>
2020-06-15 14:21:09 -0700

    added event listners handling file upload and proceeding to next step

--- client/comp/Step1_FileUpload.jsx
+++ client/comp/Step1_FileUpload.jsx
@@ -1,19 +1,47 @@
 // node packages
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
 // styled components
 import { StepIcon } from './style.jsx';
 
-export default function Step1_FileUpload() {
+export default function Step1_FileUpload( {step, setStep} ) {
+  let [buttonDisabled, setButtonDisabled ] = useState(true);
+  let fileInputRef = useRef()
+  
+  let iconTheme = (step === 1) ? "material-icons" : "material-icons-outlined";
+
+  let uploadFileOnClick = () => {
+    let data = new FormData();
+    data.append('audio', fileInputRef.current.files[0]);
+
+    axios.post('/api/v1/file', data, {
+      header: {
+        'Content-Type': 'multipart/form-data',
+      }
+    })
+      .then(() => {
+        setStep(2);
+        setButtonDisabled(true);
+      })
+      .catch((e) => {
+        console.log(e);
+      }); 
+  }
+
+  let enableButtonOnChange = () => {
+    setButtonDisabled(false)
+  }
+ 
   return (
     <div>
-      <h5><StepIcon className="material-icons">looks_one</StepIcon>Upload audio file you want edit</h5>
+      <h5><StepIcon className={iconTheme} navbar={false}>looks_one</StepIcon>Upload audio file you want edit</h5>
       <Form>
         <Form.Group controlId="audioFilePath">
-          <Form.Control type="file" accept="audio/*" />
+          <Form.Control ref={fileInputRef} type="file" accept="audio/*" onChange={enableButtonOnChange}/>
         </Form.Group>
-        <Button variant="outline-info" size="sm">Upload File</Button>
+        <Button variant="outline-info" size="sm" onClick={uploadFileOnClick} disabled={buttonDisabled}>Upload File</Button>
       </Form>
     </div>
   )
@@ -34,6 +62,7 @@ export default function Step1_FileUpload() {
 //   }
 // );
 
-// ChooseFile.propTypes = {
-//   uploadFileOnClick: PropTypes.func
-// }
+Step1_FileUpload.propTypes = {
+  step: PropTypes.number,
+  setStep: PropTypes.func
+}
