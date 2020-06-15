5f2ce1330dab72be1523a36a947f7cd119566331
Albert <asgk912@gmail.com>
2020-06-15 13:12:01 -0700

    added functionality to go back to welcome page

--- client/comp/NavigationBar.jsx
+++ client/comp/NavigationBar.jsx
@@ -1,22 +1,23 @@
 // node packages
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 // styled components
 import { NavBar, MusicIcon, StepIcon } from './style.jsx';
 
-export default function NavigationBar({ step }) {
+export default function NavigationBar({ step, pageControlOnClick }) {
   const [musicIcon, setMusicIcon] = useState(true);
 
-  // changeIconOnMouse = () => {
-    
-  // }
+  let changeMusicIconOnMouse = () => {
+    setMusicIcon(!musicIcon);
+  }
 
   return (
     <NavBar>
-      <MusicIcon className="material-icons-outlined" fs={"36px"}>library_music</MusicIcon>
-      {/* {musicIcon ? <MusicIcon className="material-icons-outlined" style={{position: 'absolute', left: '20px', top: '10px'}}>library_music</MusicIcon> :
-                  <MusicIcon className="material-icons" style={{position: 'absolute', left: '20px', top: '10px'}}>library_music</MusicIcon>} */}
-      
+      {musicIcon ? <MusicIcon className="material-icons-outlined" fs="36px" onMouseEnter={changeMusicIconOnMouse}>library_music</MusicIcon> :
+                  <OverlayTrigger placement="right" overlay={<Tooltip>Back to the welcome page.</Tooltip>} >
+                    <MusicIcon className="material-icons" fs="36px" onClick={pageControlOnClick} onMouseLeave={changeMusicIconOnMouse}>library_music</MusicIcon>
+                  </OverlayTrigger>}
       {step === 0 ? <StepIcon className="material-icons step1">looks_one</StepIcon> :
                     <StepIcon className="material-icons-outlined step1">looks_one</StepIcon>}
       <StepIcon className="material-icons">more_horiz</StepIcon>
@@ -34,5 +35,5 @@ export default function NavigationBar({ step }) {
 
 NavigationBar.propTypes = {
   step: PropTypes.number,
-  dispControlOnClick: PropTypes.func
+  pageControlOnClick: PropTypes.func
 };
