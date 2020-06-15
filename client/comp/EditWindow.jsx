c787a95cdc7b5eb5094b13e5da43cd144ad41158
Albert <asgk912@gmail.com>
2020-06-15 16:01:17 -0700

    import and use new react element submodule

--- client/comp/EditWindow.jsx
+++ client/comp/EditWindow.jsx
@@ -5,9 +5,10 @@ import PropTypes from 'prop-types';
 import NavigationBar from './NavigationBar.jsx';
 import Step1_FileUpload from './Step1_FileUpload.jsx';
 import Step2_SearchInfo from './Step2_SearchInfo.jsx';
+import Step3_SelectTags from './Step3_SelectTags.jsx';
 
 export default function EditWindow({ pageControlOnClick }) {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
 
   let nextStepOnClick = () => {
     setStep(step+1);
@@ -17,8 +18,8 @@ export default function EditWindow({ pageControlOnClick }) {
     <div>
       <NavigationBar step={step} pageControlOnClick={pageControlOnClick} />
       <Step1_FileUpload step={step} setStep={setStep} />
-      {(step > 1) ? <Step2_SearchInfo nextStepOnClick={nextStepOnClick} /> : ''}
-      {(step > 2) ? <div>Step 3</div> : ''}
+      {(step > 1) ? <Step2_SearchInfo step={step} setStep={setStep} nextStepOnClick={nextStepOnClick} /> : ''}
+      {(step > 2) ? <Step3_SelectTags step={step} setStep={setStep} nextStepOnClick={nextStepOnClick} /> : ''}
       {(step > 3) ? <div>Step 4</div> : ''}
     </div>
   );
