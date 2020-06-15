3af2f604617d211d1c65f811e9496ca9d5c19c46
Albert <asgk912@gmail.com>
2020-06-15 15:18:46 -0700

    added search by iTunes api

--- client/comp/Step2_SearchInfo.jsx
+++ client/comp/Step2_SearchInfo.jsx
@@ -1,11 +1,25 @@
 // node packages
 import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
+// styled components
+import { StepIcon } from './style.jsx';
+//submodule
+import SearchForm from './SearchForm.jsx';
 
-export default function Step2_SearchInfo( {nextStepOnClick} ) {
-  return (<div>Step 2 Initial</div>);
+export default function Step2_SearchInfo( {step, setStep, nextStepOnClick} ) {
+  
+  let iconTheme = (step === 2) ? "material-icons" : "material-icons-outlined";
+
+  return (
+    <div>
+      <h5><StepIcon className={iconTheme} instList={true}>looks_two</StepIcon>Search Information</h5>
+      <SearchForm setStep={setStep} nextStepOnClick={nextStepOnClick} />
+    </div>
+  );
 }
 
 Step2_SearchInfo.propTypes = {
+  step: PropTypes.number,
+  setStep: PropTypes.func,
   nextStepOnClick: PropTypes.func
 }
