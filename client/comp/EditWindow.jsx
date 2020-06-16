f2ea0a224fa6e17247d6e5cd6f2da1420b056bd1
Albert <asgk912@gmail.com>
2020-06-16 06:44:44 -0700

    changed key names and removed unnecessary information from response data of get request

--- client/comp/EditWindow.jsx
+++ client/comp/EditWindow.jsx
@@ -10,22 +10,39 @@ import Step3_SelectTags from './Step3_SelectTags.jsx';
 
 export default function EditWindow({ pageControlOnClick }) {
   const [step, setStep] = useState(2);
-  let searchedData = [];
+  let searchedData = null;
 
-  let nextStepOnClick = () => {
-    setStep(step+1);
-  }
+  // let nextStepOnClick = () => {
+  //   setStep(step+1);
+  // }
 
   let searchOnITunesAPI = (artist, title) => {
     let config = {}
     config.params = {
       term: `${artist.trim()} ${title.trim()}`
     }
-    console.log(config);
     axios.get('/api/v1/search', config)
       .then((res) => {
-        searchedData = res.data;
-        console.log(searchedData);
+        searchedData = Array(res.data.length);
+
+        for(var i=0; i<searchedData.length; i++) {
+          searchedData[i] = {};
+
+          // general info
+          searchedData[i].artist = res.data[i].artistName;
+          searchedData[i].album = res.data[i].collectionName;
+          searchedData[i].title = res.data[i].trackName;
+          searchedData[i].date = res.data[i].releaseDate;
+          searchedData[i].genre = res.data[i].primaryGenreName;
+          searchedData[i].trackNumber = res.data[i].trackNumber + '/' + res.data[i].trackCount;
+          searchedData[i].partOfSet = res.data[i].discNumber + '/' + res.data[i].discCount;
+
+          // url related
+          searchedData[i].artistViewUrl = res.data[i].artistViewUrl;
+          searchedData[i].trackViewUrl = res.data[i].trackViewUrl;
+          searchedData[i].image = res.data[i].artworkUrl100;
+        }
+
         setStep(3);
       })
       .catch((e) => console.log(e));
