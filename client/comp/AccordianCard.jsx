442011427d4ae0fd92de3989e9f71b91260e724a
Albert <asgk912@gmail.com>
2020-06-16 16:01:19 -0700

    module handles checkbox on change

--- client/comp/AccordianCard.jsx
+++ client/comp/AccordianCard.jsx
@@ -1,9 +1,22 @@
 // node packages
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Accordion, Row, Col, Card, Form, Image } from 'react-bootstrap';
+import { Accordion, Row, Col, Card, Image, Button } from 'react-bootstrap';
 
-export default function AccordianCard({ info, index }) {
+export default function AccordianCard({ info, index, submitOnClick }) {
+  let selectedInfo = {}
+  selectedInfo.artist = info.artist;
+  selectedInfo.title = info.title;
+
+  let handleCBOnChange = (e) => {
+    let targetValue = e.target.value
+    if(selectedInfo[targetValue] === undefined) {
+      selectedInfo[targetValue] = info[targetValue];
+    } else {
+      delete selectedInfo[targetValue];
+    }
+  }
+
   return (
     <Card>
       <Card.Header>
@@ -15,20 +28,22 @@ export default function AccordianCard({ info, index }) {
         <Card.Body>
           <Row>
             <Col>
-              <Form.Check type="checkbox" disabled checked label={"Artist: " + info.artist} />
-              <Form.Check type="checkbox" label={"Album: " + info.album} />
-              <Form.Check type="checkbox" disabled checked label={"Title: " + info.title} />
-              <Form.Check type="checkbox" label={"Release Date: " + info.date} />
-              <Form.Check type="checkbox" label={"Genre: " + info.genre} />
-              <Form.Check type="checkbox" label={"Track Number: " + info.trackNumber} />
-              <Form.Check type="checkbox" label={"Disc Number: " + info.partOfSet} />
+              <label><input type="checkbox" disabled checked/> Artist: {info.artist}</label> <br/>
+              <label><input type="checkbox" value="album" onChange={handleCBOnChange}/> Album: {info.album}</label> <br/>
+              <label><input type="checkbox" disabled checked/> Title: {info.title}</label>
+              <label><input type="checkbox" value="date" onChange={handleCBOnChange}/> Release Date: {info.date}</label> <br/>
+              <label><input type="checkbox" value="genre" onChange={handleCBOnChange}/> Genre: {info.genre}</label> <br/>
+              <label><input type="checkbox" value="trackNumber" onChange={handleCBOnChange}/> Track Number: {info.trackNumber}</label> <br/>
+              <label><input type="checkbox" value="partOfSet" onChange={handleCBOnChange}/> Disc Number: {info.partOfSet}</label>
             </Col>
             <Col>
-              <Image src={info.image} thumbnail /> <br/>
+              <label><input type="checkbox" value="image" onChange={handleCBOnChange}/> Album Image:</label> <br/>
+              <Image src={info.image} thumbnail /> <br/><br/>
               <a target="_blank" rel="noreferrer noopener" href={info.artistViewUrl}>to artist page on iTunes</a> <br/>
-              <a target="_blank" rel="noreferrer noopener" href={info.trackViewUrl}>to album and track page on iTunes</a>
+              <a target="_blank" rel="noreferrer noopener" href={info.trackViewUrl}>to album and track page on iTunes</a> <br/>
             </Col>
           </Row>
+          <Button variant="outline-info" size="sm" onClick={()=> {submitOnClick(selectedInfo)}}>Submit Selection</Button>
         </Card.Body>
       </Accordion.Collapse>
     </Card>
@@ -37,5 +52,6 @@ export default function AccordianCard({ info, index }) {
 
 AccordianCard.propTypes = {
   info: PropTypes.object,
-  index: PropTypes.number
+  index: PropTypes.number,
+  submitOnClick: PropTypes.func
 }
