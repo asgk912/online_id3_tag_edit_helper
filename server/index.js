d7166fd478daea3f09f0b88205e924abc49a2f70
Albert <asgk912@gmail.com>
2020-06-19 16:37:31 -0700

    added different endpoints reponses handling edit id3 tag and donwload file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
 const morgan = require('morgan');
@@ -11,44 +12,25 @@ const port = 3000;
 app.set('port', port);
 
 // parsing and logging
-app.use(express.urlencoded({'extended': true}));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 // specify the directory of static files
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
 
 // handle requests
-// to post of audio file
+// to handle audio file post
 const upload = multer();
-var audioBuffer;
-var audioFileName;
-var audioMimeType;
+
+var audioBuffer, audioFileName, audioMimeType, newTags;
 
 app.post('/api/v1/file', upload.single('audio'), (req, res) => {
   audioBuffer = req.file.buffer;
   audioFileName = req.file.originalname;
   audioMimeType = req.file.mimetype;
+  
   res.sendStatus(201);
 });
-
-app.get('/api/v1/file', (req, res) => {
-  let tags = {
-    title: "I CHANGED IT",
-    artist: "I CHANGED IT",
-    album: "I CHANGED IT",
-  }
-
-  NodeID3.write(tags, audioBuffer, (err, newBuffer) => {
-    if(err) {
-      console.log(err);
-      res.sendStatus(500);
-    } else {
-      res.set('Content-Type', audioMimeType);
-      res.set('Content-Disposition', `attachment; filename=${audioFileName}` );
-      res.end(newBuffer);
-    }
-  });
-})
 
 app.get('/api/v1/search', (req, res) => {
   let config = {};
@@ -66,7 +48,27 @@ app.get('/api/v1/search', (req, res) => {
       console.log(e);
       res.sendStatus(500);
     });
+});
+
+app.post('/api/v1/selection', (req, res) => {
+  newTags = Object.assign({}, req.body);
+
+  NodeID3.write(newTags, audioBuffer, (err, newAudioBuffer) => {
+    if(err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      audioBuffer = newAudioBuffer;
+      res.sendStatus(200);
+    }
+  });
 })
+
+app.get('/api/v1/file', (req, res) => {
+  res.set('Content-Type', audioMimeType);
+  res.set('Content-Disposition', `attachment; filename=${audioFileName}` );
+  res.end(audioBuffer);
+});
 
 // start server
 app.listen(port, () => {
