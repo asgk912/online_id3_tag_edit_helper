4beb56ce3dc55a5fc6bc7729bbcd0888e2611476
Albert <asgk912@gmail.com>
2020-06-19 16:35:42 -0700

    sends http request to edit the ID3 tag of previously uploaded audio file

--- client/comp/Step3_SelectTags.jsx
+++ client/comp/Step3_SelectTags.jsx
@@ -1,26 +1,29 @@
 // node packages
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Accordion, Row, Col, Card, Form, Image, ButtonGroup, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { Accordion } from 'react-bootstrap';
 // styled components
 import { StepIcon } from './style.jsx';
 // submodule
 import AccordianCard from './AccordianCard.jsx';
 
-export default function Step3_SelectTags({ step, infoData }) {
+export default function Step3_SelectTags({ step, setStep, infoData }) {
   let iconTheme = (step === 3) ? "material-icons" : "material-icons-outlined";
-  
-  // let accArray = Array(infoData.length);
 
-  // for(let i=0; i<infoData.length; i++) {
-  //   accArray[i] = <AccordianCard key={i} info={accArray[i]} index={i}/>
-  // }
+  let submitOnClick = (selectedInfo) => {
+    axios.post('/api/v1/selection', selectedInfo)
+      .then(() => {
+        setStep(4);
+      })
+      .catch((e) => console.log(e));
+  }
 
   return (
     <div>
       <h5><StepIcon className={iconTheme} instList={true}>looks_3</StepIcon>Select Tags</h5>
       <Accordion>
-        {infoData.map((info, index) => <AccordianCard key={index} info={info} index={index}/>)}
+        {infoData.map((info, index) => <AccordianCard key={index} info={info} index={index} submitOnClick={submitOnClick}/>)}
       </Accordion>
     </div>
   );
@@ -28,5 +31,6 @@ export default function Step3_SelectTags({ step, infoData }) {
 
 Step3_SelectTags.propTypes = {
   step: PropTypes.number,
+  setStep: PropTypes.func,
   infoData: PropTypes.array
 }
