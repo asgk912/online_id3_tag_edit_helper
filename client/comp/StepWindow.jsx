af976119a96966343599ca9c763c79b186a64eae
Albert <asgk912@gmail.com>
2020-06-21 22:29:41 -0700

    added alert features if no search result or if network errors

--- client/comp/StepWindow.jsx
+++ client/comp/StepWindow.jsx
@@ -13,25 +13,43 @@ import { Alert } from 'react-bootstrap';
 import { OverflowDiv, ScrollWidthControlDiv, StepsContainer } from './style.jsx';
 
 export default function StepWindow({ pageControlOnClick }) {
-  // Hook
+  /* 
+    Hooks
+  */
+  // state to control which steps are shown
   const [step, setStep] = useState(1);
-  const [uploadable, setUploadable] = useState(true);
-  const [infoData, setInfoData] = useState([{title: undefined}]);
+
+  // states associated with alerts on certain actions
+  const [alertUL, setAlertUL] = useState(false);
+  const [alertSearch, setAlertSearch] = useState(false);
+  const [alertNWErr,setAlertNWErr] = useState(false);
+
+  // states passed down to step 3 and 4
+  const [infoData, setInfoData] = useState([]);
   const [dlOption, setDLOption] = useState({original: 'original.mp3', artistTitle: '(artist) - (title).mp3', extension: '.mp3'});
 
-  const stepRefs = Array(5).fill(0).map(() => useRef(null)); // ref used to scroll window
+   // Refs to scroll window
+  const stepRefs = Array(5).fill(0).map(() => useRef(null));
 
   /*
-    Event Listners about http request
+    Event Listners with http request
+      other event listners are declared within each step submodule
   */
+
   // for Step 1: upload file to server
   let uploadFileOnClick = (e, fileInputRef, cb, cbIn) => {
     e.preventDefault();
 
+    if(alertNWErr){ // hide network alert if shown
+      setAlertNWErr(false); 
+    }
+
     let audioFile = fileInputRef.current.files[0];
     
     if(audioFile.size < 12000000) { // check the size of file
-      setUploadable(true); // hide alert if it is shown
+      if(alertUL){ // hide file size alert if shown
+        setAlertUL(false); 
+      }
 
       // set form data for post
       let data = new FormData();
@@ -44,26 +62,31 @@ export default function StepWindow({ pageControlOnClick }) {
         }
       })
         .then(() => {
-          if(step === 1) {
-            setStep(2); // load step 2 on browser
-          } else if (step === 4) {
-            setStep(3); // hide donwload step when 
+          // control which step to be shown based
+          if(step === 1) {            // file is uploaded for the first time
+            setStep(2);               // show step 2 only
+          } else if (step === 4) {    // have progressed up to step 4 already
+            setStep(3);               // keep step 2 and 3 and hide step 4
           }
           
-          cb(cbIn); // callback function will disable the submit button
+          // disable the submit button (such function is declared in step 1 submodule)
+          cb(cbIn);
 
           // scroll to step 2
           stepRefs[0].current.scrollTo(0, stepRefs[2].current.offsetTop - stepRefs[1].current.offsetTop + 20);
         })
-        .catch((err) => console.log(err));
-    } else {
-      setUploadable(false);
-      // setStep(1.5);
+        .catch(() => { setAlertNWErr(true); });
+    } else { // file exceeds the limit, show alert
+      setAlertUL(true);
     }
   }
  
   // for Step 2: send http request about search and sets infoData which will be passed down to step 3 
   let searchOnITunesAPI = (e, artist, title) => {
+    if(alertNWErr){ // hide network alert if shown
+      setAlertNWErr(false); 
+    }
+
     if(title) {
       e.preventDefault();
 
@@ -73,38 +96,50 @@ export default function StepWindow({ pageControlOnClick }) {
         params: {term: `${artist.trim()} ${title.trim()}`}
       })
         .then((res) => {
-          let morphed = Array(res.data.length);
+          if(res.data.length > 1) { // if proper search results are received
+            if(alertSearch){ // hide search alert if shown
+              setAlertSearch(false); 
+            }
 
-          for(var i=0; i<morphed.length; i++) {
-            morphed[i] = {};
-
-            // morph general info
-            morphed[i].artist = res.data[i].artistName;
-            morphed[i].album = res.data[i].collectionName;
-            morphed[i].title = res.data[i].trackName;
-            morphed[i].year = res.data[i].releaseDate.substring(0,10);
-            morphed[i].genre = res.data[i].primaryGenreName;
-            morphed[i].trackNumber = res.data[i].trackNumber + '/' + res.data[i].trackCount;
-            morphed[i].partOfSet = res.data[i].discNumber + '/' + res.data[i].discCount;
-
-            // morph url related
-            morphed[i].artistViewUrl = res.data[i].artistViewUrl;
-            morphed[i].trackViewUrl = res.data[i].trackViewUrl;
-            morphed[i].image = res.data[i].artworkUrl100;
+            let morphed = Array(res.data.length);
+            
+            for(var i=0; i<morphed.length; i++) {
+              morphed[i] = {};
+              
+              // morph general info
+              morphed[i].artist = res.data[i].artistName;
+              morphed[i].album = res.data[i].collectionName;
+              morphed[i].title = res.data[i].trackName;
+              morphed[i].year = res.data[i].releaseDate.substring(0,10);
+              morphed[i].genre = res.data[i].primaryGenreName;
+              morphed[i].trackNumber = res.data[i].trackNumber + '/' + res.data[i].trackCount;
+              morphed[i].partOfSet = res.data[i].discNumber + '/' + res.data[i].discCount;
+              
+              // morph url related
+              morphed[i].artistViewUrl = res.data[i].artistViewUrl;
+              morphed[i].trackViewUrl = res.data[i].trackViewUrl;
+              morphed[i].image = res.data[i].artworkUrl100;
+            }
+            
+            setInfoData(morphed); // set infoData
+            setStep(3); // go to step 3
+            
+            // scroll to step 3
+            stepRefs[0].current.scrollTo(0, stepRefs[3].current.offsetTop - stepRefs[1].current.offsetTop + 20);
+          } else {
+            setAlertSearch(true);
           }
-
-          setInfoData(morphed); // set infoData
-          setStep(3); // go to step 3
-
-          // scroll to step 3
-          stepRefs[0].current.scrollTo(0, stepRefs[3].current.offsetTop - stepRefs[1].current.offsetTop + 20);
         })
-        .catch((err) => console.log(err));
+        .catch(() => { setAlertNWErr(true); });
       }
   };
 
   // for Step 3: send selected tag data to edit the id3 tag of uploade file
   let submitTagSelection = (selectedTags) => {
+    if(alertNWErr){ // hide network alert if shown
+      setAlertNWErr(false); 
+    }
+
     axios({ // send selected tags to server
       url: '/api/v1/selection',
       method: 'post',
@@ -124,11 +159,15 @@ export default function StepWindow({ pageControlOnClick }) {
         // scroll to step 4
         stepRefs[0].current.scrollTo(0, stepRefs[4].current.offsetTop - stepRefs[1].current.offsetTop + 20);
       })
-      .catch((err) => console.log(err));
+      .catch(() => { setAlertNWErr(true); });
   }
 
   // for Step 4: modify filename and download file
   let downloadOnClick = (e, fileName) => {
+    if(alertNWErr){ // hide network alert if shown
+      setAlertNWErr(false);
+    }
+
     if(fileName){
       e.preventDefault();
 
@@ -138,7 +177,7 @@ export default function StepWindow({ pageControlOnClick }) {
         data: { fileName }
       })
         .then(()=> window.open('/api/v1/file')) // download file
-        .catch((err) => console.log(err));
+        .catch(() => { setAlertNWErr(true); });
     }
   }
 
@@ -150,13 +189,29 @@ export default function StepWindow({ pageControlOnClick }) {
         <ScrollWidthControlDiv>
           <StepsContainer>
             <Step1_FileUpload forwardRef={stepRefs[1]} step={step} uploadFileOnClick={uploadFileOnClick} />
-            {!uploadable ? <Alert variant="danger">The size of the file exceeds 12 MB</Alert> : ''}
+            {alertUL ? <Alert variant="danger">The size of the file exceeds 12 MB</Alert> : ''}
+
             {(step > 1) ? <Step2_SearchInfo forwardRef={stepRefs[2]} step={step} searchOnITunesAPI={searchOnITunesAPI} /> : ''}
+            {(step > 1 && alertSearch) ? <Alert variant="danger">No search restult.</Alert> : ''}
+
             {(step > 2) ? <Step3_SelectTags forwardRef={stepRefs[3]} step={step} infoData={infoData} submitTagSelection={submitTagSelection}/> : ''}
+
             {(step > 3) ? <Step4_Download forwardRef={stepRefs[4]} step={step} dlOption={dlOption} downloadOnClick={downloadOnClick} /> : ''}
             
             {/* extra white space to make step 3 and 4 in view*/}
             {(step > 2) ? <span><br/><br/><br/><br/></span> : ''}
+
+            {/* alert on network error */}
+            {!alertNWErr ? '' : 
+              <Alert variant="danger" 
+                    style={{position: "absolute", right: "18px", bottom: 0, width: "350px", height: "90px"}}
+                    onClose={() => setAlertNWErr(false)}
+                    dismissible>
+                <Alert.Heading>Network Error</Alert.Heading>
+                <div>Please try again in a few moments</div>
+              </Alert>
+            }
+
           </StepsContainer>
         </ScrollWidthControlDiv>
       </OverflowDiv>
